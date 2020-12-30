156aaf4478e40e04376635e2162e37046bd65775
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-30 11:12:19 +0100

    Fase 1

---
+++ EjercicioM3/EjercicioM3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EjercicioM3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1, string2, string3, string4, string5, string6;
+
+            Console.WriteLine("Escriu una ciutat");
+            string1 = Console.ReadLine();
+            Console.WriteLine("Escriu una ciutat");
+            string2 = Console.ReadLine();
+            Console.WriteLine("Escriu una ciutat");
+            string3 = Console.ReadLine();
+            Console.WriteLine("Escriu una ciutat");
+            string4 = Console.ReadLine();
+            Console.WriteLine("Escriu una ciutat");
+            string5 = Console.ReadLine();
+            Console.WriteLine("Escriu una ciutat");
+            string6 = Console.ReadLine();
+
+            Console.WriteLine("CIUTATS:");
+            Console.WriteLine(string1);
+            Console.WriteLine(string2);
+            Console.WriteLine(string3);
+            Console.WriteLine(string4);
+            Console.WriteLine(string5);
+            Console.WriteLine(string6);
+
+
+        }
+    }
+}
